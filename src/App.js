6d7ff25045618e878012434805a32ee5fc02eb6e
Florentina Ardean <florentina.ardean@hey.car>
2021-09-26 17:35:44 +0200

    added board,pokemon components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React from 'react';
 import './App.css';
+
+
+class Pokemon extends React.Component {
+
+  render() {
+    let i = this.props.value;
+    return (<li key={i}>{i}</li>);
+  }
+}
+
+class Board extends React.Component {
+  render() {
+    let pokemons = [];
+
+    for(let i= 0; i < this.props.size; i++) {
+      pokemons.push(<Pokemon 
+                        value={i}
+                        />) ;
+    }
+
+    return (
+      <div><ul>{pokemons}</ul></div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div >hello</div>
+    <Board size = "10"/>
   );
 }
 
