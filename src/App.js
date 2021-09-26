c65267ad9f760d2ed91ec9602b27faf464f8ec95
Florentina Ardean <florentina.ardean@hey.car>
2021-09-26 22:19:12 +0200

    refactor: move PokemonList and PokemonTypes to functions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './App.css';
 import GetPokemonsData from './Data.js'
 
-class PokemonTypes extends React.Component {
-  render() {
+
+class Pokemon extends React.Component {
+  getPokemonTypesComponent(types) {
     let typesComponents = [];
-    this.props.types.forEach(type => {
+    types.forEach(type => {
       typesComponents.push(<span className="type">{type.name}</span>);
     });
     return typesComponents
   }
-}
 
-class Pokemon extends React.Component {
   render() {
     let pokemon = this.props.pokemon;
 
@@ -20,21 +19,8 @@ class Pokemon extends React.Component {
       <div className="item">
         <div><img src={pokemon.sprites.front_default} alt={pokemon.name}></img></div>
         <div className="name">{pokemon.id} {pokemon.name}</div>
-        <div><PokemonTypes types={pokemon.types}/></div>
+        <div>{this.getPokemonTypesComponent(pokemon.types)}</div>
       </div>);
-  }
-}
-
-class PokemonList extends React.Component {
-  render() {
-    let pokemons = this.props.data;
-    let pokemonsComponentList = []
-
-    pokemons.forEach(pokemon => {
-      pokemonsComponentList.push(<Pokemon pokemon={pokemon} />);
-    });
-
-    return pokemonsComponentList;
   }
 }
 
@@ -48,13 +34,23 @@ class Board extends React.Component {
   componentDidMount() {
     GetPokemonsData().then(res => this.setState({ data: res.allPokemon }));
   }
+
+  getPokemonListComponent(pokemons) {
+    let pokemonsComponentList = []
+    pokemons.forEach(pokemon => {
+      pokemonsComponentList.push(<Pokemon pokemon={pokemon} />);
+    });
+
+    return pokemonsComponentList;
+  }
+
   render() {
     return (
       <div>
         <div className="header">
           <div><h1>Pokedex</h1></div>
         </div>
-        <div className="container"><PokemonList data={this.state.data} /></div>
+        <div className="container">{this.getPokemonListComponent(this.state.data)}</div>
         <div className="footer">by Flo</div>
       </div>
     )
